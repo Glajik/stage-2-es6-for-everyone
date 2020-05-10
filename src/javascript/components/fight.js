11e4cf2ceac42e5c83f41fc06a9ecd8183c8e1a5
Viacheslav Glazhevskyi <glajik@gmail.com>
2020-05-11 01:34:32 +0300

    Call fight function at arena.js Add event listener to document

--- src/javascript/components/fight.js
+++ src/javascript/components/fight.js
@@ -3,6 +3,23 @@ import { controls } from '../../constants/controls';
 export async function fight(firstFighter, secondFighter) {
   return new Promise((resolve) => {
     // resolve the promise with the winner when fight is over
+    
+    const onKeyDown = (event) => {
+      console.log(event.code);
+      switch (event.code) {
+        case controls.PlayerOneAttack:
+          
+          break;
+      
+        default:
+          break;
+      }
+
+    };
+
+    document.addEventListener('keydown', event => onKeyDown(event), true);
+
+    resolve(firstFighter);
   });
 }
 
