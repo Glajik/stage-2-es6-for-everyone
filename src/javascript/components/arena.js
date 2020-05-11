0dc40671b7c4433850a5cd99ca9221f75cdc973f
Viacheslav Glazhevskyi <glajik@gmail.com>
2020-05-11 05:32:21 +0300

    Show modal, when player win (closes #5)

--- src/javascript/components/arena.js
+++ src/javascript/components/arena.js
@@ -1,6 +1,7 @@
 import { createElement } from '../helpers/domHelper';
 import { createFighterImage } from './fighterPreview';
 import { fight } from './fight';
+import { showWinnerModal } from './modal/winner';
 
 export function renderArena(selectedFighters) {
   const root = document.getElementById('root');
@@ -13,7 +14,7 @@ export function renderArena(selectedFighters) {
   // - start the fight
   // - when fight is finished show winner
   fight(...selectedFighters)
-    .then(winner => console.log('Show modal - Winer:', winner.name))
+    .then(winner => showWinnerModal(winner))
     .catch(error => console.error(error));
 
 }
--- src/javascript/components/modal/winner.js
+++ src/javascript/components/modal/winner.js
@@ -1,3 +1,30 @@
+import { showModal } from './modal';
+import { createElement } from '../../helpers/domHelper';
+import { createFighterImage } from '../fighterPreview';
+
+// call showModal function 
 export function showWinnerModal(fighter) {
-  // call showModal function 
+  const { name } = fighter;
+
+  const image = createFighterImage(fighter);
+
+  const bodyElement = createElement({
+    tagName: 'div',
+    attributes: {
+      style: `
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        width: 50vw;
+        height: 50vh;
+      `
+    }
+  });
+
+  bodyElement.append(image);
+
+  showModal({
+    title: `Winner is ${name}! Good game!!`,
+    bodyElement,
+  });
 }
